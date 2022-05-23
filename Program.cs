ea650a5b7c4ee109f5231db304587978a327da41
Davi Melo <davisantosmelo2006@gmail.com>
2022-05-23 16:35:03 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace WebsiteChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----------Verifique se o site está online----------");
+            Console.WriteLine("Digite a URL do site...");
+
+            var URL = Console.ReadLine();
+            Verify(URL);
+
+            static void Verify(string? URL)
+            {
+                HttpClient Client = new HttpClient();
+                try
+                {
+                    var res = Client.GetAsync(URL);
+                    var status = res.Result.IsSuccessStatusCode;
+                    if (status)
+                        Console.WriteLine("O site está online!");
+                    else
+                        Console.WriteLine("O site está offline!");
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("URL inválida.");
+
+                }
+            }
+        }
+    }
+}
