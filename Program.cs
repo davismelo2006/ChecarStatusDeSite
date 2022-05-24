2beed0e0d5fa20ab443a639f82ec5b332dfdc4b0
Davi Melo <davisantosmelo2006@gmail.com>
2022-05-24 14:58:35 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -9,24 +9,22 @@
 
             var URL = Console.ReadLine();
             Verify(URL);
-
-            static void Verify(string? URL)
+        }
+        static void Verify(string? URL)
+        {
+            HttpClient Client = new HttpClient();
+            try
             {
-                HttpClient Client = new HttpClient();
-                try
-                {
-                    var res = Client.GetAsync(URL);
-                    var status = res.Result.IsSuccessStatusCode;
-                    if (status)
-                        Console.WriteLine("O site está online!");
-                    else
-                        Console.WriteLine("O site está offline!");
-                }
-                catch (System.Exception)
-                {
-                    Console.WriteLine("URL inválida.");
-
-                }
+                var res = Client.GetAsync(URL);
+                var status = res.Result.IsSuccessStatusCode;
+                if (status)
+                    Console.WriteLine("O site está online!");
+                else
+                    Console.WriteLine("O site está offline!");
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("URL inválida.");
             }
         }
     }
